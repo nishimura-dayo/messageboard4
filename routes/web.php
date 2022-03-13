ab25b5bd05f31ef13b0518ee95183a07e7d149d0
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2022-03-13 00:27:04 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// CRUD
+// // メッセージの個別詳細ページ表示
+// Route::get('messages/{id}', 'MessagesController@show');
+// // メッセージの新規登録を処理
+// Route::post('messages', 'MessagesController@store');
+// // メッセージの更新処理
+// Route::put('messages/{id}', 'MessagesController@update');
+// //メッセージを削除
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// // index: showの補助ページ
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+// // create: 新規作成用のフォームページ
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+// // edit: 更新用のフォームページ
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
+
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
