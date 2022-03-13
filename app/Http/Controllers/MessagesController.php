931258e929219c4ef8daeade6bd3739a0a58cf04
nabecchi <92443197+nishimura-dayo@users.noreply.github.com>
2022-03-13 13:34:02 +0000

    validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -50,6 +50,11 @@ class MessagesController extends Controller
     // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         // メッセージを作成
         $message = new Message;
         $message->content = $request->content;
@@ -105,6 +110,12 @@ class MessagesController extends Controller
     //putまたはpatchでmessages/(任意のid)にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
+    
         // idの値でメッセージを検索して取得
         $message = Message::findOrFail($id);
         // メッセージを更新
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,12 @@
 
     <body>
         {{-- ナビゲーションバー --}}
-        @include('commons..navbar')
+        @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
 
